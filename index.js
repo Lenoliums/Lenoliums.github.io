44a4361e580878a1218cb162fb7d775f8517dbcd
Lenoliums <elena.borisova0210@yandex.ru>
2024-08-04 19:12:21 +0300

    projectCards animation and links

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ document.addEventListener('scroll', skillsAnimation)
 document.addEventListener('scroll', headerAboutAnimation)
 document.addEventListener('scroll', headerPortfolioAnimation)
 document.addEventListener('scroll', infoAnimation)
+document.addEventListener('scroll', projectCardsAnimation)
+
 
 function navAnimation(){
     const nav = document.getElementsByTagName('nav')[0];
@@ -66,7 +68,7 @@ function skillAnimation(){
 }
 function skillsAnimation(){
     const skills= document.getElementById('skills');
-    if(isVisible(skills)){
+    if(isVisible(skills, 0)){
         skills.classList.add('slideinRightAnimation')
         setTimeout(skillAnimation, 1000)
     }
@@ -77,27 +79,77 @@ function headerAboutAnimation(){
     const h=document.getElementById('about-header')
     if(isVisible(h)){
         h.classList.add('slideinLeftAnimation')
-        setTimeout(()=>{document.getElementsByClassName('line')[0].classList.add('slideinLeftAnimation')}, 1000)
+        setTimeout(()=>{document.getElementsByClassName('line')[0].classList.add('slideinLeftAnimation')}, 300)
     }
 }
 function headerPortfolioAnimation(){
     const h=document.getElementById('portfolio-header')
     if(isVisible(h)){
         h.classList.add('slideinRightAnimation')
-        setTimeout(()=>{document.getElementsByClassName('line')[1].classList.add('slideinRightAnimation')}, 1000)
+        setTimeout(()=>{document.getElementsByClassName('line')[1].classList.add('slideinRightAnimation')}, 300)
     }
 }
 
 function infoAnimation(){
     const info=document.getElementById('info')
-    if(isVisible(info)){
+    if(isVisible(info, 0)){
         info.classList.add('slideinLeftAnimation')
     }
 }
 
-function isVisible(el) {
+function projectCardsAnimation(){
+    const projectCards = [...document.getElementsByClassName('projectConteiner')]
+    const firstProjectCard =projectCards[0]
+    if(isVisible(firstProjectCard, 0)){
+        let time = 0
+        const projectsImg = [...document.getElementsByClassName('projectImg')]
+        const projectsName = [...document.getElementsByClassName('projectName')]
+        const projectsTool = [...document.getElementsByClassName('projectTools')]
+        const projectsButton = [...document.getElementsByClassName('projectButton')]
+        const projectsCenters = [...document.getElementsByClassName('center')]
+        projectCards.forEach((el, ind) => {
+            setTimeout(()=>{el.classList.add('slideinBottomAnimation500')}, time)
+            el.onmouseover=()=>projectHooverCardAnimation(projectsImg[ind], projectsName[ind], projectsTool[ind], projectsButton[ind], projectsCenters[ind])
+            el.onmouseout=()=>projectUnhooverCardAnimation(projectsImg[ind], projectsName[ind], projectsTool[ind], projectsButton[ind], projectsCenters[ind])
+            time+=100
+        });
+    }
+}
+function projectHooverCardAnimation(img, name, tools, button, center){
+    img.classList.add('opacityDown')
+    center.classList.remove('opacityDown')
+    center.style.opacity=1 
+
+    name.classList.remove('slideoutTopAnimation')
+    tools.classList.remove('slideoutTopAnimation')
+    button.classList.remove('slideoutBottomAnimation')
+
+    name.classList.add('slideinTopAnimation')
+    tools.classList.add('slideinTopAnimation')
+    button.classList.add('slideinBottomAnimation')
+    
+}
+function projectUnhooverCardAnimation(img, name, tools, button, center){
+    img.classList.remove('opacityDown')
+    img.classList.add('opacityUp')
+
+    name.classList.remove('slideinTopAnimation')
+    tools.classList.remove('slideinTopAnimation')
+    button.classList.remove('slideinBottomAnimation')
+
+    name.classList.add('slideoutTopAnimation')
+    tools.classList.add('slideoutTopAnimation')
+    button.classList.add('slideoutBottomAnimation')
+    center.classList.add('opacityDown')
+
+}
+
+function isVisible(el, pos=window.innerHeight/3) {
+    if(typeof el == "undefined"){
+        return false
+    }
     var wt = document.documentElement.scrollTop;
     var tt = el.offsetTop;
     var tb = tt + el.offsetHeight;
-    return ((tb <= wt + window.innerHeight) && (tt >= wt));
+    return ((tb + pos<= wt + window.innerHeight) && (tt >= wt));
 }
--- projectCards.js
+++ projectCards.js
@@ -2,25 +2,36 @@ const projects=[
     {
         name: 'TicTacToe',
         img: 'img/tic-tac-toe.jpg',
-        tools: 'JS / CSS'
+        tools: 'JS / CSS',
+        buttonText: 'View site',
+        siteURL: "./TicTacToe/index.html",
     },
     {
         name: 'e-lern',
         img: '',
-        tools: 'JS / CSS'
+        tools: 'JS / CSS',
+        buttonText: 'View site',
+        siteURL: "./e-lern-Task/index.html",
     },
     {
         name: 'Calculator',
         img: '',
-        tools: 'React JS / CSS'
+        tools: 'React JS / CSS',
+        buttonText: 'View site',
+        siteURL: "./Calculator/",
+    },
+    {
+        name: 'Calculator',
+        img: '',
+        tools: 'React JS / CSS',
+        buttonText: 'Learn more',
+        siteURL: "./Calculator/",
     }
 ]
 function projectsCardsLoading(){
-    console.log(document)
     const projectsConteiner= document.getElementById('portfolioConteiner')
-    console.log(projectsConteiner)
-    let projectsCards=projects.map(project=>`<div class='projectConteiner'><img class='projectImg' src=${project.img} alt=${project.name}><div class="center"><h2 class='projectName'>${project.name}</h2><h3 class='projectTools'>${project.tools}</h3><div class="projectButton">Learn more</div></div></div>`)
-    console.log(projectsCards)
+    let projectsCards=projects.map(project=>`<div class='projectConteiner'><img class='projectImg' src=${project.img} alt=${project.name}><div class="center"><h2 class='projectName'>${project.name}</h2><h3 class='projectTools'>${project.tools}</h3>
+    <div class="projectButton">${project.buttonText=='View site'?'<a href='+project.siteURL+'>View site</a>':project.buttonText}</div></div></div>`)
     projectsConteiner.innerHTML=projectsCards;
     }
 window.addEventListener("load", projectsCardsLoading);
