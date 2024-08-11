70f2df77bf00d1e3e230251e7230658c1f53e877
Lenoliums <elena.borisova0210@yandex.ru>
2024-08-11 22:29:28 +0300

    projectInfoCard append

--- projectCards.js
+++ projectCards.js
@@ -5,6 +5,8 @@ const projects=[
         tools: 'JS / CSS',
         buttonText: 'View site',
         siteURL: "./TicTacToe/index.html",
+        discription: '',
+        discriptionImg: []
     },
     {
         name: 'e-lern',
@@ -12,6 +14,8 @@ const projects=[
         tools: 'JS / CSS',
         buttonText: 'View site',
         siteURL: "./e-lern-Task/index.html",
+        discription: '',
+        discriptionImg: []
     },
     {
         name: 'Calculator',
@@ -19,6 +23,8 @@ const projects=[
         tools: 'React JS / CSS',
         buttonText: 'View site',
         siteURL: "./Calculator/",
+        discription: '',
+        discriptionImg: []
     },
     {
         name: 'Calculator',
@@ -26,12 +32,20 @@ const projects=[
         tools: 'React JS / CSS',
         buttonText: 'Learn more',
         siteURL: "./Calculator/",
+        discription: 'project discription',
+        discriptionImg: ['p']
     }
 ]
 function projectsCardsLoading(){
     const projectsConteiner= document.getElementById('portfolioConteiner')
     let projectsCards=projects.map(project=>`<div class='projectConteiner'><img class='projectImg' src=${project.img} alt=${project.name}><div class="center"><h2 class='projectName'>${project.name}</h2><h3 class='projectTools'>${project.tools}</h3>
-    <div class="projectButton">${project.buttonText=='View site'?'<a href='+project.siteURL+'>View site</a>':project.buttonText}</div></div></div>`)
+    <div class="projectButton">${project.buttonText=='View site'?'<a href='+project.siteURL+'>View site</a>':'<div onclick="projectCardDiscription('+`'${project.discription}'`+','+ `'${project.discriptionImg}'`+','+ `'${project.name}'`+')">'+project.buttonText+'</div>'}</div></div></div>`)
     projectsConteiner.innerHTML=projectsCards;
     }
+
+function projectCardDiscription(discriptionText, discriptionImg, name){
+    document.getElementsByClassName('projectDiscriptionConteiner')[0].style.visibility= 'visible';
+    console.log(discriptionText)
+
+}    
 window.addEventListener("load", projectsCardsLoading);
